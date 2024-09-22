b017aec7ef45dbadeb2d8f4a1d3c3e55586ccf65
master-craftsman <75668100+master-craftsman@users.noreply.github.com>
2024-09-22 16:33:25 +0600

    Update birge.py

--- birge.py
+++ birge.py
@@ -21,7 +21,7 @@ pass_birge = os.getenv('PASS_BIRGE')
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Логин/Email" and @class="login"]'))).send_keys(email_birge)
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Пароль" and @class="pass"]'))).send_keys(pass_birge)
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@value="Войти" and @class="submit"]'))).click()
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '(//a[@class="sb_p_lk_link"])[2]'))).click()
+WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//a[text()="Мой кабинет"]'))).click()
 
 # Поиск и нажатие кнопок "Поднять объявление"
 while True:
